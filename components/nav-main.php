384b82939025d71ead719c138aa78afb1a2b2f9b
Juanillo <sr.jifernandez@gmail.com>
2015-09-19 11:19:48 +0200

    Components

---
+++ components/nav-main.php
@@ -0,0 +1,9 @@
+<?php
+ wp_nav_menu( array(
+    'theme_location' => 'main-menu', // Setting up the location for the main-menu, Main Navigation.
+    'menu_class' => 'nav', //Adding the class for dropdowns
+    'container_id' => 'navwrap', //Add CSS ID to the containter that wraps the menu.
+    'fallback_cb' => 'wp_page_menu', //if wp_nav_menu is unavailable, WordPress displays wp_page_menu function, which displays the pages of your blog.
+    )
+);
+?>
